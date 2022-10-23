ca52c42c280efff6f206b98fc30dce8e8f235ad2
super-code-world <csecec.1802044@gmail.com>
2022-10-23 16:00:53 +0530

    Initial Development

---
+++ index.js
@@ -0,0 +1,61 @@
+// Click on screen
+for(var i = 0; i< document.querySelectorAll(".drum").length; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var buttonHTML = this.innerHTML;
+    sound(buttonHTML);
+    buttonAnimation(buttonHTML);
+  });
+}
+
+// Key pressing
+document.addEventListener("keypress", function(event){
+  var key = event.key;
+  sound(key);
+  buttonAnimation(key);
+});
+// function for making sound;
+function sound(key){
+
+  switch(key){
+    case "w":
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
+    default:
+     console.log("Error");
+      break;
+  }
+
+}
+
+function buttonAnimation(key){
+  document.querySelector("."+key).classList.add("pressed");
+
+  setTimeout(function(){
+    document.querySelector("."+key).classList.remove("pressed");
+  }, 100);
+}
